1c41efe13d28651b4ad5de39512975d5a9c7ced0
ThomasH-W <thomas@hoeser.info>
2017-12-28 18:27:49 +0100

    2 - update

--- password.h
+++ password.h
@@ -9,15 +9,15 @@
 #define PASSWORD_H_
 
 
-#define WIFI_SSID   "Aletheia"
-#define WIFI_PASS   "Wahrheit"
+#define WIFI_SSID   "myssis"
+#define WIFI_PASS   "mysecretpass"
 
 #define ACCESS_POINT_NAME  "ESP-Accesspoint"
 #define ACCESS_POINT_PASSWORD  "12345678"
 #define AdminTimeOut 180  // Defines the Time in Seconds, when the Admin-Mode will be disabled
 
-#define UPDATE_USER_NAME  "root"
-#define UPDATE_USER_PASSWORD  "nxt2008"
+#define UPDATE_USER_NAME  "myroot"
+#define UPDATE_USER_PASSWORD  "mysecretpass"
 
 
 #endif /* PASSWORD_H_ */
